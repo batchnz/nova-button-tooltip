a6e9de830a354247f82eaa142d1097ba5825020a
Daniel Siemers <daniel.siemers@gmail.com>
2024-04-24 16:31:51 +1200

    fix(buttons): stop propagation

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="inline-flex items-center">
+  <div class="inline-flex items-center" @click="handleClick">
     <span class="inline-flex" v-for="button of field.buttons">
       <a
         class="cursor-pointer text-70 hover:text-primary mr-3 inline-flex items-center has-tooltlip"
@@ -20,6 +20,12 @@ export default {
     fieldValue() {
       return this.field.displayedAs || this.field.value
     },
-  }
+  },
+
+  methods: {
+    handleClick(e) {
+      e.stopPropagation();
+    },
+  },
 }
 </script>
