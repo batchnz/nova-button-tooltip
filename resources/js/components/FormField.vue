d0f247c71defc23e641935e4c09ef31b395c8cec
Matthias Law <matthias@refineddigital.co.nz>
2024-12-18 16:38:25 +1300

    fix: remove unnecessary imports

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -3,27 +3,7 @@
 </template>
 
 <script>
-import { FormField, HandlesValidationErrors } from 'laravel-nova'
-
 export default {
-  mixins: [FormField, HandlesValidationErrors],
-
   props: ['resourceName', 'resourceId', 'field'],
-
-  methods: {
-    /*
-     * Set the initial, internal value for the field.
-     */
-    setInitialValue() {
-      this.value = this.field.value || ''
-    },
-
-    /**
-     * Fill the given FormData object with the field's internal value.
-     */
-    fill(formData) {
-      formData.append(this.fieldAttribute, this.value || '')
-    },
-  },
 }
 </script>
